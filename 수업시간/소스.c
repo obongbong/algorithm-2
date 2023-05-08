bc6c4def8f67e43e8e17a8f44ce509d855b693ce
obongbong <127162394+obongbong@users.noreply.github.com>
2023-05-08 12:52:45 +0900

    Add files via upload

--- 수업시간/소스.c
+++ 수업시간/소스.c
@@ -7,11 +7,38 @@ typedef struct _Nodeinfo{
 	char* name[20];
 }Node;
 
+const Node nodeSet[4] = { {2332, "n0"}, {9993, "n1"}, {332, "n2"}, {234, "n3"} }; //Node 집합
+
+typedef struct _Bnode {
+	int id;
+	Bnode* left;
+	Bnode* right;
+}Bnode;
+
+//반환: 초기화된 이진 트리의 루트노드
+Bnode initTree()
+{
+	Bnode* b0 = (Bnode*)malloc(sizeof(Bnode));
+	Bnode* b1 = (Bnode*)malloc(sizeof(Bnode));
+	Bnode* b2 = (Bnode*)malloc(sizeof(Bnode));
+	Bnode* b3 = (Bnode*)malloc(sizeof(Bnode));
+	Bnode* b4 = (Bnode*)malloc(sizeof(Bnode));
+
+	b0->left = b1;
+	b1->right = b2;
+	b2->left = b3;
+	b2->right = b4;
+	
+	// ...
+
+}return b0;
+
+
 int main(void)
 {
 	//const char* nodeName[4] = { "n0", "n1", "n2", "n3" }; //Node 집합
 
-	const Node nodeSet[4] = { {2332, "n0"}, {9993, "n1"}, {332, "n2"}, {234, "n3"} }; //Node 집합
+	//const Node nodeSet[4] = { {2332, "n0"}, {9993, "n1"}, {332, "n2"}, {234, "n3"} }; //Node 집합
 
 	int G[4][4] = {
 		{0, 3, INFN, INFN},
@@ -24,6 +51,12 @@ int main(void)
 
 }
 
+void initGraph()
+{
+	Node n1;
+	strcpy(n1.name, "n0");
+	nodeSet[0] = n1;
 
+}
 
 
