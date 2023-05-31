17e65fae910d47a991dca08a1389a0e10459923e
obongbong <127162394+obongbong@users.noreply.github.com>
2023-05-31 16:44:26 +0900

    Add files via upload

---
+++ 배낭문제/이주형_202011228_2배낭문제.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+typedef struct _Item {
+	int number;
+	int weight;
+	int price;
+}Item;
+
+void Knapsack(int C, int i, int wi, int vi)
+{
+	int w, n;
+	if (i = 0 || w = 0) return;
+
+	else if (i != 0)
+	{
+		for (w = 1; w < n; w++)
+		{
+			if (wi > w)
+			{
+				
+			}
+		}
+	}
+}
+
+int main()
+{
+	return 0;
+}
